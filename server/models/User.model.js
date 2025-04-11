bebb71b68b0cbdf7f2ffc6fe43ed6d8a2da1eba5
capa026 <carlospacheco.fx@gmail.com>
2025-04-11 11:35:00 -0400

    FIX User.findByEmail

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -3,15 +3,16 @@ import { db } from "../config/db.js";
 class User {
   static async create({ name, lastName, email, password }) {
     const { rows } = await db.query(
-      "INSERT INTO users (name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *"
+      "INSERT INTO users (name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, lastName, email, password]
     );
     return rows[0];
   }
 
   static async findByEmail(email) {
-    const { rows } = await db.query(
-      `SELECT * FROM users WHERE email = ${email}`
-    );
+    const { rows } = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
     return rows[0];
   }
