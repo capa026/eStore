a6259a280b27d1747e9ea37fe02cff14082c81aa
capa026 <carlospacheco.fx@gmail.com>
2025-04-11 10:45:13 -0400

    UPDATE index.js auth route endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import AuthRoute from "./routes/auth.route.js";
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", AuthRoute);
 
 app.listen(PORT, () => {
   console.log("Server Running on Port:", PORT);
