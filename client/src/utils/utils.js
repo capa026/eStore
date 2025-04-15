0d33b14fa86f70a7890cb726c1664dc37c6e9ad4
capa026 <carlospacheco.fx@gmail.com>
2025-04-15 07:10:51 -0400

    ADD Static routes

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -5,3 +5,17 @@ export const detectDeviceType = () => {
 
   return device;
 };
+
+export const appRoutes = [
+  { category: "Videojuegos_y_consolas", title: "Videojuegos y consolas" },
+  { category: "Computacion", title: "Computacion" },
+  { category: "Telefonos_y_accesorios", title: "Telefonos y accesorios" },
+  { category: "Accesorios_y_Miscelaneos", title: "Accesorios y Miscelaneos" },
+  { category: "Ropa_para_damas", title: "Ropa para damas" },
+  { category: "Ropa_para_caballeros", title: "Ropa para caballeros" },
+  { category: "Todo_para_tu_bebe", title: "Todo para tu bebe" },
+  {
+    category: "Accesorios_de_moto_y_bicicleta",
+    title: "Accesorios de moto y bicicleta",
+  },
+];
