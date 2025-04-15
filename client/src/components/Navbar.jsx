5bf5e33031bed7e9896f07bea013c2e45875e83d
capa026 <carlospacheco.fx@gmail.com>
2025-04-15 10:26:08 -0400

    UPDATE Logo link to / path

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,16 +6,15 @@ const Navbar = () => {
   return (
     <nav className="bg-gradient-to-br from-primary/50 to-primary/30 p-2 shadow-bottom-sm sticky top-0 backdrop-blur-md">
       <div className="container mx-auto flex items-center justify-between gap-4">
-        <img
-          src={titleSvg.src}
-          alt="eStore - title"
-          className="hidden w-24 md:block"
-        />
-        <img
-          src={logoSvg.src}
-          alt="eStore - title"
-          className="block w-8 md:hidden"
-        />
+        <a
+          href="/"
+          className="hidden hover:bg-black/20 p-2 rounded-md md:block"
+        >
+          <img src={titleSvg.src} alt="eStore - title" className="w-24" />
+        </a>
+        <a href="/" className="active:bg-black/20 p-2 rounded-md md:hidden">
+          <img src={logoSvg.src} alt="eStore - title" className="block w-8" />
+        </a>
         <InputIcon icon={<Search />} placeholder={"Busca tus productos aqui"} />
         <div className="cursor-pointer p-1 rounded-md border-2 border-black/50 ">
           <Menu />
